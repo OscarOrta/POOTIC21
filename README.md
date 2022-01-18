3491d12fb1c809116df573153be83888aa85cda8
OSCAR ORTA <97772582+OscarOrta@users.noreply.github.com>
2022-01-18 16:38:02 +0000

    programa 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #PROGRAMACIÓN ORIENTADA A OBJETOS 
-
 1.programa_01.py - hola mundo
+2.programa_02.py - suma
+3.programa_03.py
---
+++ programa_03.py
@@ -0,0 +1,13 @@
+enteros = 6
+flotante = 4.56
+cadena = "hola"
+boleano = True
+print(enteros)
+print(flotante)
+print(cadena)
+print(boleano)
+
+print(type(enteros))
+print(type(flotante))
+print(type(cadena))
+print(type(boleano))
